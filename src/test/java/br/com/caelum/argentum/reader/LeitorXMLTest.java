5a7ed60de9ea67da536b5a93d859d1bd6344c4fe
Andressa Albuquerque <andressa.dsa2@gmail.com>
2014-07-23 22:25:09 -0300

    More tests

--- src/test/java/br/com/caelum/argentum/reader/LeitorXMLTest.java
+++ src/test/java/br/com/caelum/argentum/reader/LeitorXMLTest.java
@@ -37,4 +37,53 @@ public class LeitorXMLTest {
 		assertEquals(1000, n.getQuantidade(), 0.00001);
 	}
 
+	@Test
+	public void carregaXmlComZeroNegociacoes() {
+		String xmlDeTeste = "<list>" +
+				"			</list>";
+		
+		LeitorXML leitor = new LeitorXML();
+		
+		InputStream xml = new ByteArrayInputStream(xmlDeTeste.getBytes());
+		
+		List<Negociacao> negociacoes = leitor.carrega(xml);
+		
+		assertEquals(0, negociacoes.size());
+	}
+	
+	@Test
+	public void carregaXmlComTresNegociacoes() {
+		String xmlDeTeste = "<list>" +
+				"				<negociacao>" +
+				"					<preco>43.5</preco>" +
+				"					<quantidade>1000</quantidade>" +
+				"					<data>" +
+				"						<time>1322233344455</time>" +
+				"					</data>" +
+				"				</negociacao>" +
+				"				<negociacao>" +
+				"					<preco>43.5</preco>" +
+				"					<quantidade>1000</quantidade>" +
+				"					<data>" +
+				"						<time>1322233344455</time>" +
+				"					</data>" +
+				"				</negociacao>" +
+				"				<negociacao>" +
+				"					<preco>43.5</preco>" +
+				"					<quantidade>1000</quantidade>" +
+				"					<data>" +
+				"						<time>1322233344455</time>" +
+				"					</data>" +
+				"				</negociacao>" +
+				"			</list>";
+		
+		LeitorXML leitor = new LeitorXML();
+		
+		InputStream xml = new ByteArrayInputStream(xmlDeTeste.getBytes());
+		
+		List<Negociacao> negociacoes = leitor.carrega(xml);
+		
+		assertEquals(3, negociacoes.size());
+		
+	}
 }
